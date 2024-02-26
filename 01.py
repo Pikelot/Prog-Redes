bae706a3eeac730b9ae85217509838c220e44266
joao <silvrt3d@gmail.com>
2024-02-26 15:42:02 -0300

    organizei

--- 01.py
+++ 01.py
@@ -2,23 +2,23 @@ import gerar_lista
 import sys
 
 try:
+    #variáveis
     quant = int(input('Insira a quantidade: '))
     val_min = int(input('Insira o valor mínimo: '))
     val_max = int(input('Insira o valor máximo: '))
-
+    
+    #geração de lista
     try:
-        lista = gerar_lista.gerar_lista(quant, val_min, val_max)
+            lista = gerar_lista.gerar_lista(quant, val_min, val_max)
 
-        if not lista[0]:
-            print(lista[1])
-            sys.exit
-        else: 
-            print(lista[1])
+            #printar a lista ou não
+            if not lista[0]:
+                print(lista[1])
+                sys.exit
+            else: 
+                print(lista[1])
+                
     except:
         print('insira os valores corretamente')
 except:
     print('insira valores inteiros')
-try:
-    lista = gerar_lista.gerar_lista(quant, val_min, val_max)
-except:
-    print('insira os valores corretamente')
