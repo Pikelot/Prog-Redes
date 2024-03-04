2105092c426d693c88ff3705bcfd9e31a4ac17c6
joao <silvrt3d@gmail.com>
2024-03-04 15:57:56 -0300

    ordenação bubble e selection feitas.

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
-lista_gerada = []
-with open(f'arquivo_teste.txt', 'r') as arquivo:
-            for item in arquivo:
-                print(item)
-                lista_gerada.append(item)
-                print (lista_gerada)
+n = len(nome_lista)
+        for i in range(n):
+            min_index = i
+            for j in range(i + 1, n):
+                if nome_lista[j] < nome_lista[min_index]:
+                    min_index = j
+            nome_lista[i], nome_lista[min_index] = nome_lista[min_index], nome_lista[i]
+        return nome_lista
