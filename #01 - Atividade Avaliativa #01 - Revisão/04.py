41920e43f603250f820263effb555c89a05d0652
Pikelot <48073507+Pikelot@users.noreply.github.com>
2024-03-26 17:32:35 -0300

    hmmm vdc

--- #01 - Atividade Avaliativa #01 - Revisão/04.py
+++ #01 - Atividade Avaliativa #01 - Revisão/04.py
@@ -1,10 +1,26 @@
 
+import random
+
 lista = []
 
-with open(f'term.txt', 'r') as arquivo:
-    for item in arquivo:
-        lista.append(item)
+#abrir o arquivo com as palavras
+try:
+    with open(f'term.txt', 'r') as arquivo:
+        for item in arquivo:
+            lista.append(item)
+    lista = [item.strip() for item in lista]
+except:
+    print('arquivo.txt não existe ou foi apagado')
 
+#escolha randômica da palavra
+#termo = random.choice(lista)
+termo = 'baiano'
 
-#desculpa se tiver palavrão na lista, as listas eram todas erradas
-print(lista)
+if len(termo) == 6:
+    ajustamento = 104
+else:
+    ajustamento = 100
+
+print(f'______|   BEM VINDO AO TERMO! |______'.center(164))
+print(f'|A palavra selecionada tem {len(termo)} letras!|'.center(164))
+print(f'|{"|_____|" * len(termo)}|'.rjust(ajustamento))
