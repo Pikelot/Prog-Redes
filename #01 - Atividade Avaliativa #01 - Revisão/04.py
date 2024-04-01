380a8b1c2f73c9cae2cb9e45c3ab28167f57e5a1
joao <silvrt3d@gmail.com>
2024-04-01 15:47:19 -0300

    ta certo

--- #01 - Atividade Avaliativa #01 - Revisão/04.py
+++ #01 - Atividade Avaliativa #01 - Revisão/04.py
@@ -2,6 +2,8 @@
 import random
 
 lista = []
+rodada = 1
+
 
 #abrir o arquivo com as palavras
 try:
@@ -14,13 +16,44 @@ except:
 
 #escolha randômica da palavra
 #termo = random.choice(lista)
-termo = 'baiano'
-
-if len(termo) == 6:
-    ajustamento = 104
-else:
+termo = 'motos'
+#ajustamento para embelezamento da tabela
+if len(termo) == 5:
     ajustamento = 100
+elif len(termo) == 6:
+    ajustamento = 104
+elif len(termo) == 7:
+    ajustamento = 107
+elif len(termo) == 8:
+    ajustamento = 110
 
-print(f'______|   BEM VINDO AO TERMO! |______'.center(164))
-print(f'|A palavra selecionada tem {len(termo)} letras!|'.center(164))
-print(f'|{"|_____|" * len(termo)}|'.rjust(ajustamento))
+letras_corretas = []
+letras_incorretas = []
+while True:
+    print(f'______|   BEM VINDO AO TERMO! |______'.center(164))
+    print(f'|A palavra selecionada tem {len(termo)} letras!|'.center(164))
+    for i in range(0, rodada):
+        cont = 0
+        for letra in termo:
+            if letra in letras_corretas:
+                print(f'| {letra} ', end='')
+                cont += 1
+            else:
+                print('|___', end='')
+                cont += 1
+            
+            if cont == len(termo):
+                if letra in letras_corretas:
+                    print(f'| {letra} ', end='|')
+                else:
+                    print('|___ ', end='|')
+                    
+    print('|'.rjust(ajustamento))
+    try:
+        escolha = input('Digite um termo: ')
+    except:
+        print('Numeros não são letras!!!') 
+
+print(letras_corretas)
+print(letras_incorretas)
+print(termo)
