4d5659cb26ecbe9b5376ab72c60388b1efdaa1fa
joao <silvrt3d@gmail.com>
2024-04-01 15:47:26 -0300

    sim

--- #01 - Atividade Avaliativa #01 - Revisão/teste.py
+++ #01 - Atividade Avaliativa #01 - Revisão/teste.py
@@ -1,24 +1,49 @@
-import hashlib
-import time
+import random
 
-def findNonce(dataToHash, bitsToBeZero):
-    start_time = time.time() # Registra o tempo de início
-    nonce = 0
-    while True:
-        # Concatena o nonce ao conjunto de bytes
-        data = dataToHash + nonce.to_bytes((nonce.bit_length() + 7) // 8, 'big')
-        # Calcula o hash SHA256
-        hash_result = hashlib.sha256(data).hexdigest()
-        # Converte o hash em binário e verifica os bits iniciais
-        if hash_result.startswith('0' * bitsToBeZero):
-            end_time = time.time() # Registra o tempo de término
-            elapsed_time = end_time - start_time # Calcula o tempo decorrido
-            return nonce, elapsed_time
-        nonce += 1
+# Carrega palavras do arquivo
+try:
+    with open('term.txt', 'r') as arquivo:
+        lista = [linha.strip() for linha in arquivo]
+except FileNotFoundError:
+    print('O arquivo term.txt não existe ou foi apagado.')
 
-# Exemplo de uso
-data = b"Esse e facil"
-bits_to_be_zero = 8
-nonce, time_taken = findNonce(data, bits_to_be_zero)
-print("Nonce encontrado:", nonce)
-print("Tempo decorrido (segundos):", time_taken)
+# Escolha randômica da palavra
+if lista:
+    termo = random.choice(lista)
+else:
+    termo = 'motos'
+
+# Ajustamento para embelezamento da tabela
+ajustamento = 100 + (len(termo) - 5) * 4
+
+# Inicializa listas para letras corretas e incorretas
+letras_certas = []
+letras_incorretas = []
+
+# Loop principal do jogo
+while True:
+    print(f'______|   BEM VINDO AO TERMO! |______'.center(164))
+    print(f'|A palavra selecionada tem {len(termo)} letras!|'.center(164))
+
+    # Desenha espaços para as letras corretas e incorretas
+    for letra in termo:
+        if letra in letras_certas:
+            print(f'| {letra} ', end='')
+        else:
+            print('|___ ', end='')
+    print('|'.rjust(ajustamento))
+
+    # Pede a palavra ao jogador
+    escolha = input('Digite um termo: ')
+
+    # Verifica se cada letra da escolha do jogador está na palavra selecionada
+    for letra in escolha:
+        if letra in termo:
+            letras_certas.append(letra)
+        else:
+            letras_incorretas.append(letra)
+
+    # Verifica se o jogador adivinhou a palavra corretamente
+    if set(letras_certas) == set(termo):
+        print("Parabéns! Você adivinhou a palavra corretamente!")
+        break
