82a0e50c60564c57cdc28c3282ee5000f3849cda
joao <silvrt3d@gmail.com>
2024-07-17 15:47:16 -0300

    Consertei mais slc muito gêmio

--- #04 - Estudo Dirigido #02 - NetScan/Questão 03.py
+++ #04 - Estudo Dirigido #02 - NetScan/Questão 03.py
@@ -1,8 +1,8 @@
 import socket, sys
 
-def dividir(file):
+def dividir(arquivo):
     portas = []
-    for line in file:
+    for line in arquivo:
         # Dividindo a linha no primeiro '/'
         parts = line.split('/', 1)
         if len(parts) == 2:
@@ -26,57 +26,59 @@ strHost = input('Digite a url: ')
 #strHost = 'www.ifrn.edu.br'
 try:
     ipHost  = socket.gethostbyname(strHost)
+    error = 1
 except:
     print('Não use http:// ou https://')
-#Abrindo o arquivo e executando a função de dividir as portas em Numero, Tipo e Descrição
+    error = 0
 
+#Abrindo o arquivo e executando a função de dividir as portas em Numero, Tipo e Descrição
 with open('#04 - Estudo Dirigido #02 - NetScan/portas.txt','r') as arquivo:
     pasta = dividir(arquivo)
 
 #testando e printando os resultados para cada porta
+if error == 1:
+    for porta in pasta:
 
-for porta in pasta:
-    
-    if "TCP" and "UDP" in porta[1]:
-        #Criando um socket em tcp e outro em udp
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        sock2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        if "TCP" and "UDP" in porta[1]:
+            #Criando um socket em tcp e outro em udp
+            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+            sock2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-        try:
-            sock.connect((ipHost, int(porta[0])))
-            statust = "ligada"
-        except:
-            statust = "fechada"
-        try:
-            sock2.connect((ipHost, int(porta[0])))
-            statusu = "ligada"
-        except:
-            statusu = "fechada"
+            try:
+                sock.connect((ipHost, int(porta[0])))
+                statust = "ligada"
+            except:
+                statust = "fechada"
+            try:
+                sock2.connect((ipHost, int(porta[0])))
+                statusu = "ligada"
+            except:
+                statusu = "fechada"
 
-        print(f'Porta {porta[0]}: Protocolo TCP,UDP: {porta[2]} / Status: TCP: {statust} UDP: {statusu}')
+            print(f'Porta {porta[0]}: Protocolo TCP,UDP: {porta[2]} / Status: TCP: {statust} UDP: {statusu}')
 
-        #sock.close
-    
-    elif "TCP" in porta[1]:
+            #sock.close
 
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        elif "TCP" in porta[1]:
 
-        try:
-            sock.connect((ipHost, int(porta[0])))
-            statust = "ligada"
-        except:
-            statust = "fechada"
-        
-        print(f'Porta {porta[0]}: Protocolo TCP: {porta[2]} / Status: TCP: {statust}')
+            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
-    else:
+            try:
+                sock.connect((ipHost, int(porta[0])))
+                statust = "ligada"
+            except:
+                statust = "fechada"
+            
+            print(f'Porta {porta[0]}: Protocolo TCP: {porta[2]} / Status: TCP: {statust}')
 
-        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        else:
 
-        try:
-            sock.connect((ipHost, int(porta[0])))
-            statust = "ligada"
-        except:
-            statust = "fechada"
-        
-        print(f'Porta {porta[0]}: Protocolo UDP: {porta[2]} / Status: UDP: {statust}')
+            sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+            try:
+                sock.connect((ipHost, int(porta[0])))
+                statust = "ligada"
+            except:
+                statust = "fechada"
+            
+            print(f'Porta {porta[0]}: Protocolo UDP: {porta[2]} / Status: UDP: {statust}')
